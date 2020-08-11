dd5751045a84f0009d3433010ba3c7c01368c6b9
Fly me to the moon <git@bigly.dog>
2020-08-11 02:21:21 -0700

    extend

--- lua/narc_t9/clients.lua
+++ lua/narc_t9/clients.lua
@@ -10,7 +10,7 @@ local init = function (location)
   local lines = fn.readfile(config)
   local json = table.concat(lines, "")
   local data = fn.json_decode(json)
-  vim.g.narc_settings_private = data
+  vim.g.narc_settings_private = vim.tbl_extend("force", (vim.g.narc_settings_private or {}), data)
 
 end
 
