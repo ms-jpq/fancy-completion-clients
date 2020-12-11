ad33a9750831f0f1c84bc795d6034474642db2dc
Fly me to the moon <git@bigly.dog>
2020-12-10 21:57:20 -0800

    lint

--- lint.sh
+++ lint.sh
@@ -6,8 +6,25 @@ set -o pipefail
 
 cd "$(dirname "$0")" || exit 1
 
-FILES=(
-  ./rplugin/python3/kok_t9/*.py
-  )
 
-mypy --ignore-missing-imports -- "${FILES[@]}"
+readarray -t -d $'\0' TRACKED < <(git ls-files --exclude-standard -z)
+readarray -t -d $'\0' UNTRACKED < <(git ls-files --exclude-standard --others -z)
+
+ALL_FILES=("${TRACKED[@]}" "${UNTRACKED[@]}")
+PYTHON_FILES=()
+for FILE in "${ALL_FILES[@]}";
+do
+  case "$FILE" in
+    *.py)
+      if [[ -f "$FILE" ]]
+      then
+        PYTHON_FILES+=("$PWD/$FILE")
+      fi
+      ;;
+    *)
+      ;;
+  esac
+done
+
+
+mypy -- "${PYTHON_FILES[@]}"
